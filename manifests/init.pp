c52eda9af48a47739ce552d8fedd30ee67177651
Alex Cline <acline@us.ibm.com>
2013-04-16 15:59:08 -0400

    Added dependencies for sys-filesystem gem -- needs native libs and build environment.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,8 +8,13 @@
 # take two puppet runs for the facts to be correctly populated.
 
 class disk_stats {
-  exec { 'Install sys-filesystem rubygem':
+  if ! defined ( Package['ruby-devel'] ) { package { 'ruby-devel': ensure => installed, } }
+  if ! defined ( Package['make'] )       { package { 'make':       ensure => installed, } }
+  if ! defined ( Package['gcc-c++'] )    { package { 'gcc-c++':    ensure => installed, } }
+
+  exec { 'Install sys-filesystem rubygem dependency':
     command => '/bin/env gem install sys-filesystem',
     unless  => '/bin/env gem list --local | /bin/grep sys-filesystem',
+    require => [ Package['ruby-devel'], Package['make'], Package['gcc-c++'], ],
   }
 }
