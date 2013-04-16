3ac28d7991a1159b595d278d99d8e4c31b04b992
Alex Cline <acline@us.ibm.com>
2013-04-16 15:41:31 -0400

    Added more docs.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,10 @@
 # This class isn't required to use the disk_stats facts.  It will
 # install the required sys-filesystem gem.
 
+# Note, this will install the gem after the first time the fact is
+# executed.  This means if the gem isn't already installed, it will
+# take two puppet runs for the facts to be correctly populated.
+
 class disk_stats {
   exec { 'Install sys-filesystem rubygem':
     command => '/bin/env gem install sys-filesystem',
