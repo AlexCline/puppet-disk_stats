b58edb627383dacfd7a88afbf5044c59dcc8cedd
Alex Cline <acline@us.ibm.com>
2013-04-17 11:42:48 -0400

    Added support for multiple platforms.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,13 +8,28 @@
 # take two puppet runs for the facts to be correctly populated.
 
 class disk_stats {
-  if ! defined ( Package['ruby-devel'] ) { package { 'ruby-devel': ensure => installed, } }
-  if ! defined ( Package['make'] )       { package { 'make':       ensure => installed, } }
-  if ! defined ( Package['gcc-c++'] )    { package { 'gcc-c++':    ensure => installed, } }
+  if ! defined ( Package['rubygems'] )   { package { 'rubygems':   ensure => installed, } }
+
+  case $::operatingsystem {
+    'CentOS', 'RedHat': {
+      if ! defined ( Package['ruby-devel'] ) { package { 'ruby-devel': ensure => installed, } }
+      if ! defined ( Package['make'] )       { package { 'make':       ensure => installed, } }
+      if ! defined ( Package['gcc-c++'] )    { package { 'gcc-c++':    ensure => installed, } }
+      $require_list = [ Package['rubygems'], Package['ruby-devel'], Package['make'], Package['gcc-c++'], ]
+    }
+    default: {
+      $require_list = [ Package['rubygems'], ]
+    }
+  }
+
+  $env_path = $::operatingsystem ? {
+    /^(CentOS|RedHat)$/ => '/bin/env',
+    /^(Ubuntu|Debian)$/ => '/usr/bin/env',
+  }
 
   exec { 'Install sys-filesystem rubygem dependency':
-    command => '/bin/env gem install sys-filesystem',
-    unless  => '/bin/env gem list --local | /bin/grep sys-filesystem',
-    require => [ Package['ruby-devel'], Package['make'], Package['gcc-c++'], ],
+    command => "${env_path} gem install sys-filesystem",
+    unless  => "${env_path} gem list --local | /bin/grep sys-filesystem",
+    require => $require_list,
   }
 }
